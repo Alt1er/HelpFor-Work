0efc3d151d841d774d4b2f988b218993d242ebe2
Yehor Alt1 <evgwozdew@gmail.com>
2025-01-25 22:07:05 +0200

    v1.2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const openBlocks = document.querySelectorAll('.openBlock'),
       btnOpen = document.querySelectorAll('.openButton'), 
       checkBtns = document.querySelectorAll('[data-check]'), 
-      blockTasks = document.querySelectorAll('.blockTasks');
+      blockTasks = document.querySelectorAll('.blockTasks'), 
+      navLinks = document.querySelectorAll('[data-switch]'),
+      mainBlocks = document.querySelectorAll('[data-main]');
 
 // Attach event listeners to each openButton
 btnOpen.forEach((btn, index) => {
@@ -10,11 +12,17 @@ btnOpen.forEach((btn, index) => {
     });
 });
 
-
-
 checkBtns.forEach((btn, index1) => {
     btn.addEventListener('click', () =>{
         blockTasks[index1].classList.toggle('checked')
     });
-})
+});
 
+
+navLinks.forEach((btn, index) => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        mainBlocks.forEach((block) => block.classList.remove('showBlock'));
+        mainBlocks[index].classList.add('showBlock');
+    });
+});
